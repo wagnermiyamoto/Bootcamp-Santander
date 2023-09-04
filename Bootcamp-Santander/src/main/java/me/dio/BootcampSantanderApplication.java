595b5780bd5116e8222d3b5133198f39a7a42640
Wagner <wagner_miyamoto@hotmail.com>
2023-09-04 20:17:56 -0300

    Initial commit

---
+++ Bootcamp-Santander/src/main/java/me/dio/BootcampSantanderApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampSantanderApplication.class, args);
+	}
+
+}
